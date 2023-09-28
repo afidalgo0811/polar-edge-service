66278fa0ac1a2f1b928549792a99b8fed9097898
afidalgoSE <afidalgose@gmail.com>
2023-09-28 17:38:09 -0400

    wip

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,44 +1,53 @@
+import com.diffplug.gradle.spotless.SpotlessExtension
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "3.1.4"
-	id("io.spring.dependency-management") version "1.1.3"
-	kotlin("jvm") version "1.8.22"
-	kotlin("plugin.spring") version "1.8.22"
+  id("org.springframework.boot") version "3.1.4"
+  id("io.spring.dependency-management") version "1.1.3"
+  kotlin("jvm") version "1.8.22"
+  kotlin("plugin.spring") version "1.8.22"
+  id("com.diffplug.spotless") version "6.19.0"
 }
 
 group = "com.afidalgo"
+
 version = "0.0.1-SNAPSHOT"
 
-java {
-	sourceCompatibility = JavaVersion.VERSION_17
-}
+java { sourceCompatibility = JavaVersion.VERSION_17 }
 
-repositories {
-	mavenCentral()
-}
+repositories { mavenCentral() }
 
 extra["springCloudVersion"] = "2022.0.4"
 
 dependencies {
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("org.springframework.cloud:spring-cloud-starter-gateway")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
+  implementation("org.jetbrains.kotlin:kotlin-reflect")
+  implementation("org.springframework.cloud:spring-cloud-starter-gateway")
+  testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
 dependencyManagement {
-	imports {
-		mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
-	}
+  imports {
+    mavenBom(
+        "org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+  }
 }
 
 tasks.withType<KotlinCompile> {
-	kotlinOptions {
-		freeCompilerArgs += "-Xjsr305=strict"
-		jvmTarget = "17"
-	}
+  kotlinOptions {
+    freeCompilerArgs += "-Xjsr305=strict"
+    jvmTarget = "17"
+  }
 }
 
-tasks.withType<Test> {
-	useJUnitPlatform()
+tasks.withType<Test> { useJUnitPlatform() }
+
+configure<SpotlessExtension> {
+  kotlin {
+    // by default the target is every '.kt' and '.kts` file in the java sourcesets
+    ktfmt() // has its own section below
+  }
+  kotlinGradle {
+    target("*.gradle.kts")
+    ktfmt()
+  }
 }
--- src/main/kotlin/com/afidalgo/polaredgeservice/PolarEdgeServiceApplication.kt
+++ src/main/kotlin/com/afidalgo/polaredgeservice/PolarEdgeServiceApplication.kt
@@ -3,9 +3,8 @@ package com.afidalgo.polaredgeservice
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
-class PolarEdgeServiceApplication
+@SpringBootApplication class PolarEdgeServiceApplication
 
 fun main(args: Array<String>) {
-	runApplication<PolarEdgeServiceApplication>(*args)
+  runApplication<PolarEdgeServiceApplication>(*args)
 }
