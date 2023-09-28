fa9f03f0f62c57fa68aa41af1297d04fa3856d1b
afidalgoSE <afidalgose@gmail.com>
2023-09-28 15:22:13 -0400

    wip

---
+++ src/main/kotlin/com/afidalgo/polaredgeservice/PolarEdgeServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.afidalgo.polaredgeservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PolarEdgeServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<PolarEdgeServiceApplication>(*args)
+}
---
+++ src/test/kotlin/com/afidalgo/polaredgeservice/PolarEdgeServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.afidalgo.polaredgeservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PolarEdgeServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
