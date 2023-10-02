9dadbe85c1919b1ff5f72ea051286669376f3823
afidalgoSE <afidalgose@gmail.com>
2023-10-02 03:14:15 -0400

    added circuit breakers

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,6 +23,8 @@ dependencies {
   implementation("org.jetbrains.kotlin:kotlin-reflect")
   implementation("org.springframework.cloud:spring-cloud-starter-gateway")
   testImplementation("org.springframework.boot:spring-boot-starter-test")
+  implementation(
+      "org.springframework.cloud:spring-cloud-starter-circuitbreaker-reactor-resilience4j")
 }
 
 dependencyManagement {
